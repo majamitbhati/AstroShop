1d2c91457c6f065ea2a566ed63a1a1cddd306339
majamitbhati <majamitbhati@gmail.com>
2024-08-13 12:38:21 +0530

    Add files via upload

---
+++ src/content/config.ts
@@ -0,0 +1,32 @@
+import {z, defineCollection} from "astro:content"
+
+
+const shopsCollection = defineCollection({
+    schema: ({image}) =>
+        z.object({
+            category: z.string(),
+            date: z.string(),
+               // .transform(str => format(new Date(str), "MMMM d, yyyy")),
+            featured: z.boolean().default(false),
+            image: image(),
+            title: z.string(),
+            floor: z.string(),
+            size: z.number(),
+            price: z.number(),
+            atrium: z.boolean(),
+            corner: z.boolean(),
+            status: z.string(),
+            byowner: z.boolean(),
+            discount: z.number(),
+            brokerage: z.boolean(),
+            plc: z.string(),
+            name: z.string(),
+            contactno: z.string(),
+            giftvisit: z.string(),
+            giftbuy: z.string()
+        }),
+});
+
+export const collections = {
+    shops: shopsCollection,
+}
